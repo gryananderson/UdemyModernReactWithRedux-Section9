206b3c5105e045178989104c9a23247c024a8724
Ryan <gryananderson@gmail.com>
2018-05-14 17:55:16 -0500

    Modern React w Redux - Section 9, Lesson 125

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=afio4fna4i7evnp4n7apnpvo874nn78va8ovavovonYOUvadagagdag';
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+    return {
+        type: FETCH_POSTS,
+        payload: request
+    };
+}
