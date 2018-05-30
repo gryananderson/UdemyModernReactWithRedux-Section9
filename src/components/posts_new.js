268c3fefaa80788e303b2b9850a165097119f6bc
Ryan <gryananderson@gmail.com>
2018-05-30 16:35:29 -0500

    Modern React w Redux - Section 9, Lesson 136

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -15,13 +15,34 @@ class PostsNew extends Component {
         return (
             <form>
                 <Field label="Title" name="title" component={this.renderField}/>
-                <Field label="Tags" name="tags" component={this.renderField}/>
-                <Field label="Content" name="content" component={this.renderField}/>
+                <Field label="Categories" name="categories" component={this.renderField}/>
+                <Field label="Post Content" name="content" component={this.renderField}/>
             </form>
         )
     }
 }
 
+function validate(values) {
+    // console.log(values) -> Object { title: 'x', categories: 'y', content: 'z' }
+    const errors = {};
+
+    // Validate the inputs from the values object
+    if (!values.title || values.title.length < 3) {
+        errors.title = 'Enter a title that is at least three characters';
+    }
+    if (!values.categories) {
+        errors.categories = 'Enter some categories';
+    }
+    if (!values.content) {
+        errors.content = 'Enter some content';
+    }
+
+    // If errors is empty, the form is fine to submit - Passes validation
+    // If errors is not empty, redux-form assumes the form is invalid
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm-MustBeUnique'
 })(PostsNew);
